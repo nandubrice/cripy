fde980489b3094ea23a7d9d54861b97746e15adb
Arnado Briceño Catacora <bricenoarnaldo7@gmail.com>
2022-12-09 12:20:55 -0500

    actualizado

--- src/components/C03estilosConCss.js
+++ src/components/C03estilosConCss.js
@@ -4,7 +4,7 @@ const C03estilosConCss = () => {
     return(
         <div className='marcoComponente'>
             <h4>Componente 03 con formas de dividr estilos</h4>
-            <button style={{backgroundColor: 'black', color:"cyan", padding:"5px"}}>
+            <button style={{backgroundColor: 'red', color:"white", padding:"10px"}}>
                 Boton 1 con estilos en linea de atributos (Definido como objeto)
             </button>
             <hr/>
